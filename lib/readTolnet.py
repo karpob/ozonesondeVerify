27120d4b059d7f30f4829803e435337d098e6a8e
Bryan M. Karpowicz <bkarpowi@discover16.prv.cube>
2019-04-03 11:39:32 -0400

    replace reading converted h5 files to using hdf (4) directly.

--- lib/readTolnet.py
+++ lib/readTolnet.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py,os 
 from datetime import datetime 
+from pyhdf.SD import SD, SDC
 
 def getDatetimeFromMJD(mjdIn):
     """
@@ -15,7 +16,68 @@ def getDatetimeFromMJD(mjdIn):
     
     return np.asarray(outArray)
 
+def h4get(f,dset):
+    """
+    helper function to pull data from h4 field.
+    """
+    obj = f.select(dset)
+    return obj.get()
+
+def readTolnetH4(fname):
+    d = {}
+    profileDicts = []
+    h4 = SD(fname,SDC.READ)
+
+    d['ALT'] = np.asarray( h4get(h4,'ALTITUDE') )
+    d['Elevation'] = np.asarray( h4get(h4,"ALTITUDE.INSTRUMENT") )
+    d['startTime'] = getDatetimeFromMJD( np.asarray( h4get(h4,"DATETIME.START") ) ) 
+    d['endTime'] = getDatetimeFromMJD( np.asarray( h4get(h4,"DATETIME.STOP") ) )
+    d['dT'] = np.asarray( h4get(h4,"INTEGRATION.TIME") ) 
+    d['Latitude']= np.asarray( h4get(h4,"LATITUDE.INSTRUMENT") ) 
+    d['Longitude']= np.asarray( h4get(h4,"LONGITUDE.INSTRUMENT") )  
+    d['O3MR'] = np.asarray( h4get(h4,"O3.MIXING.RATIO.VOLUME_DERIVED") )
+    d['O3MRUncert'] = np.asarray( h4get(h4,"O3.MIXING.RATIO.VOLUME_DERIVED_UNCERTAINTY.COMBINED.STANDARD") ) 
+    d['O3ND'] = np.asarray ( h4get(h4,"O3.NUMBER.DENSITY_ABSORPTION.DIFFERENTIAL") )
+    d['Press'] = np.asarray( h4get(h4,"PRESSURE_INDEPENDENT") )
+    d['Temp'] = np.asarray( h4get(h4,"TEMPERATURE_INDEPENDENT") )
+    dims = d['O3MR'].shape
+    if(len(dims) > 1):
+        nProfiles, nLevels = dims[0],dims[1]
+        for i in range(0,nProfiles):
+            dd = {}
+            dd['startTime'] = d['startTime'][i]
+            dd['endTime'] = d['endTime'][i]
+            dd['O3MR'] = d['O3MR'][i,:]
+            dd['O3ND'] = d['O3ND'][i,:]
+            dd['Press'] = d['Press']
+            dd['Temp'] = d['Temp']
+            dd['Longitude'] = d['Longitude']
+            dd['Latitude'] = d['Latitude']
+            dd['Elevation'] = d['Elevation']
+            profileDicts.append(dd)
+    else:
+        dd = {}
+        dd['startTime'] = d['startTime']
+        dd['endTime'] = d['endTime']
+        dd['O3MR'] = d['O3MR'][:]
+        dd['O3ND'] = d['O3ND'][:]
+        dd['Press'] = d['Press']
+        dd['Temp'] = d['Temp']
+        dd['Longitude'] = d['Longitude']
+        dd['Latitude'] = d['Latitude']
+        dd['Elevation'] = d['Elevation']
+        profileDicts.append(dd)
+    # because they had to be different..same as ".close()" would be for any other api in the universe.
+    h4.end() 
+    return profileDicts
+
+
 def readTolnetH5(fname):
+    """
+    Pretty much the same as readTolnetH4, but I wrote this first. I started out converting these things
+    to hdf5, then reading them, because I really don't like the pyhdf interface. Keeping this here for 
+    the day when pyhdf gets orphaned, or the world finally agrees with me that hdf4 is just too old.
+    """
     d = {}
     profileDicts = []
     h5 = h5py.File(fname,'r')
@@ -131,7 +193,7 @@ def readTolnetAscii(fname):
 def readTolnet( h5Files, datFiles ):
     bigList = []
     for f in h5Files:
-        bigList.extend(readTolnetH5(f))
+        bigList.extend(readTolnetH4(f))
     for f in datFiles:
         bigList.extend(readTolnetAscii(f)) 
 
